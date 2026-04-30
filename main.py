2abed159645f87765511f0f4b1afc40e78f0cc54
tagciccone <tagciccone@gmail.com>
2026-04-30 17:48:44 -0400

    skeletoning

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+
+    # TODO Vehicle dynamics
+
+    # TODO Tire forces
+
+    # TODO Engine torque / Wheel torque relationship
+
+    # TODO Inverse torque map / model
+    
+    # TODO Model validation
+
+    # TODO Trajectory Generation
+
+    # TODO Tracking control
+
+    # TODO Evaluation
+    ...
